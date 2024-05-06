412c8623149e69f3af9bdcfdd7ca71b0de7c094f
fon <anatsaya.lee@gmail.com>
2024-05-07 00:12:04 +0700

    Create /duscount  api

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ webServer.use(cors());
 const corsOptions = {
   origin: "http://localhost:3000.com",
   optionsSuccessStatus: 200,
-  method: "GET",
+  method: "GET,POST",
 };
 
 const ticketListsData = [
@@ -48,9 +48,49 @@ const ticketListsData = [
   },
 ];
 
+const discountsData = [
+  {
+    code: "10%Off",
+    discount: 10,
+    type: "percentage",
+  },
+  {
+    code: "200OFF",
+    discount: 200,
+    type: "amount",
+  },
+  {
+    code: "50%Off",
+    discount: 50,
+    type: "percentage",
+  },
+];
+
 webServer.get("/ticketLists", (_, res) => {
   try {
     res.json({ ticketListsData });
+  } catch (error) {
+    console.error("Error occurs:", error);
+    res.status(500).json({ error: "Internal Server Error", status: "error" });
+  }
+});
+
+webServer.post("/discount", (req, res) => {
+  try {
+    const code = req.body.discountCode;
+    const totalPrice = req.body.totalPrice;
+
+    const discountData = discountsData.find((each) => each.code === code);
+
+    if (!discountData) {
+      return res.json({ discount: 0 });
+    }
+
+    const discount =
+      discountData.type === "percentage"
+        ? (discountData.discount / 100) * totalPrice
+        : discountData.discount;
+    return res.json({ discount });
   } catch (error) {
     console.error("Error occurs:", error);
     res.status(500).json({ error: "Internal Server Error", status: "error" });
